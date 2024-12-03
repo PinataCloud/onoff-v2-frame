85273711290cf8d87b9721f1866671f3842d6140
Steve <stevedsimkins@gmail.com>
2024-12-03 14:15:00 -0500

    fix

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,67 +1,14 @@
 "use client";
 
-import { useEffect, useCallback, useState } from "react";
-import sdk, {
-	FrameNotificationDetails,
-	type FrameContext,
-} from "@farcaster/frame-sdk";
-import {
-	useAccount,
-	useSendTransaction,
-	useSignMessage,
-	useSignTypedData,
-	useWaitForTransactionReceipt,
-	useDisconnect,
-	useConnect,
-	useSwitchChain,
-	useChainId,
-} from "wagmi";
-
-import { config } from "~/components/providers/WagmiProvider";
-import { Button } from "~/components/ui/Button";
-import { truncateAddress } from "~/lib/truncateAddress";
-import { base } from "wagmi/chains";
+import { useEffect, useState } from "react";
+import sdk from "@farcaster/frame-sdk";
 
 export default function Demo(
 	{ title }: { title?: string } = { title: "Frames v2 Demo" },
 ) {
 	const [isSDKLoaded, setIsSDKLoaded] = useState(false);
-	const [context, setContext] = useState<FrameContext>();
-	const [isContextOpen, setIsContextOpen] = useState(false);
-	const [txHash, setTxHash] = useState<string | null>(null);
-	const [addFrameResult, setAddFrameResult] = useState("");
-	const [notificationDetails, setNotificationDetails] =
-		useState<FrameNotificationDetails | null>(null);
-	const [sendNotificationResult, setSendNotificationResult] = useState("");
-
-	const { address, isConnected } = useAccount();
-	const chainId = useChainId();
-
-	const {
-		sendTransaction,
-		error: sendTxError,
-		isError: isSendTxError,
-		isPending: isSendTxPending,
-	} = useSendTransaction();
-
-	const { isLoading: isConfirming, isSuccess: isConfirmed } =
-		useWaitForTransactionReceipt({
-			hash: txHash as `0x${string}`,
-		});
-
-	const {
-		signTypedData,
-		error: signTypedError,
-		isError: isSignTypedError,
-		isPending: isSignTypedPending,
-	} = useSignTypedData();
-
-	const { disconnect } = useDisconnect();
-	const { connect } = useConnect();
-
 	useEffect(() => {
 		const load = async () => {
-			setContext(await sdk.context);
 			sdk.actions.ready();
 		};
 		if (sdk && !isSDKLoaded) {
